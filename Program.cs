d14ef2c34355478d6af16733f06f791cba757d0e
user <ivan10122003potapov@gmail.com>
2022-03-21 13:09:48 +0400

    Booba

--- Program.cs
+++ Program.cs
@@ -5,15 +5,33 @@ namespace Box
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Я программа для бла-бла-бла")
-int = x
-x=Console.ReadLine ();
-For(i=0; i<x; i++)
-{
-if (i == 0 || i == x-1)
-{
+            Console.WriteLine("Я программа для бла-бла-бла");
+            int x;
+        x=Convert.ToInt32(Console.ReadLine());//Теперь программа осзазнаёт, что "х" вводит пользователь и конвертирует его в числдо единиц, требуемыхдля постраения квадрата.
+        for(x=0; i<x; x++);
+            {
+                if (i == 0 || i == x - 1);
+                {
 
-}
+                }
+            }
         }
     }
-}
+
+//    using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//namespace box
+//    {
+//        internal class Program
+//        {
+//            static void Main(string[] args)
+//            {
+//                Console.WriteLine("Я - Руслан. Программа для вывода квадратов из нулей и единиц, где единицы есть диагонали квадрата.");
+//                string chislo = Console.ReadLine();
+//            }
+//        }
+//    }
+//
