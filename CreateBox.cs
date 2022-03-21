e725ccd848aca3fe5596a463cd51ef3666999eb0
Lite <artem.artamonov.94@bk.ru>
2022-03-21 23:04:24 +0400

    ФиналОчка

--- CreateBox.cs
+++ CreateBox.cs
@@ -10,30 +10,56 @@ namespace Box
     {
         public static string Square(int a)
         {
-            if (a % 2 != 0)
+            if (a == 1)
+            {
+                Console.WriteLine("1");
+                return "";
+            }
+            if (a > 1)
             {
                 int n = 2;
                 int u = 0;
-                for (int i = 0; i < a / 2; i++)
+                if (a % 2 != 0)
                 {
-                    Console.WriteLine(string.Concat(Enumerable.Repeat("0", i)) + "1" + 
-                        string.Concat(Enumerable.Repeat("0", a - n)) + "1" + string.Concat(Enumerable.Repeat("0", i)));
-                    n += 2;
-                    u = i;
-                }
-                Console.WriteLine(string.Concat(Enumerable.Repeat("0", a / 2)) + "1" 
-                    + string.Concat(Enumerable.Repeat("0", a / 2)));
-                n -= 2;
-                for (int i = u; i > -1; i--)
-                {
-                    Console.WriteLine(string.Concat(Enumerable.Repeat("0", i)) + "1" + string.Concat(Enumerable.Repeat("0", a - n)) + "1" + string.Concat(Enumerable.Repeat("0", i)));
+
+                    for (int i = 0; i < a / 2; i++)
+                    {
+                        Console.WriteLine(string.Concat(Enumerable.Repeat("0", i)) + "1" +
+                            string.Concat(Enumerable.Repeat("0", a - n)) + "1" + string.Concat(Enumerable.Repeat("0", i)));
+                        n += 2;
+                        u = i;
+                    }
+                    Console.WriteLine(string.Concat(Enumerable.Repeat("0", a / 2)) + "1"
+                        + string.Concat(Enumerable.Repeat("0", a / 2)));
                     n -= 2;
+                    for (int i = u; i > -1; i--)
+                    {
+                        Console.WriteLine(string.Concat(Enumerable.Repeat("0", i)) + "1" + string.Concat(Enumerable.Repeat("0", a - n)) + "1" + string.Concat(Enumerable.Repeat("0", i)));
+                        n -= 2;
+                    }
+                }
+                if (a % 2 == 0)
+                {
+                    for (int i = 0; i < a / 2; i++)
+                    {
+                        Console.WriteLine(string.Concat(Enumerable.Repeat("0", i)) + "1" +
+                            string.Concat(Enumerable.Repeat("0", a - n)) + "1" + string.Concat(Enumerable.Repeat("0", i)));
+                        n += 2;
+                        u = i;
+                    }
+                    n -= 2;
+                    for (int i = u; i > -1; i--)
+                    {
+                        Console.WriteLine(string.Concat(Enumerable.Repeat("0", i)) + "1" + string.Concat(Enumerable.Repeat("0", a - n)) + "1" + string.Concat(Enumerable.Repeat("0", i)));
+                        n -= 2;
+                    }
                 }
             }
-
-
-             return "asd";
-            
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            return "";
         }
     }
 }
@@ -43,16 +69,9 @@ namespace Box
 //0110
 //1001
 
-//10001
-//01010
-//00100
-//01010
-//10001
-
-//1000001
-//0100010
-//0010100
-//0001000
-//0010100
-//0100010
-//1000001
+//100001
+//010010
+//001100
+//001100
+//010010
+//100001
