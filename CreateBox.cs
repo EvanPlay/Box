e2ac9ff5fa2be55eca07cd0dff71407fbb56c5c2
Lite <artem.artamonov.94@bk.ru>
2022-03-21 13:35:13 +0400

    asd

---
+++ CreateBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    internal class CreateBox
+    {
+        public static string Square(int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+
+            }
+
+
+
+            return "asd";
+        }
+    }
+}
